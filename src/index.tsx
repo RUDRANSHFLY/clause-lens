fa3077bb2285acf8c954d6191dae27549c6d554e
RUDRANSH BARADIYA <mayamayavir@gmail.com>
2026-06-13 06:07:54 +0530

    feat(ui): add upload page to the root route

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
 import { Hono } from "hono";
 import { UploadPage } from "./ui/pages";
+import { logger } from "hono/logger";
 
 const app = new Hono();
+
+app.use(logger());
 
 app.get("/", (c) => {
   return c.html(<UploadPage />);
